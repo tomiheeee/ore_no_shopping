cab2a12197efbac492b6d224501eef534ff6b7fc
yuhei-tomita <tomita.yuhei@moneyforward.co.jp>
2019-07-14 12:07:02 +0900

    必要なgemの追加

---
+++ vendor/bundle/gems/rakuten_web_service-1.11.0/lib/rakuten_web_service/ichiba.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rakuten_web_service/version'
+require 'rakuten_web_service/configuration'
+
+require 'rakuten_web_service/ichiba/shop'
+require 'rakuten_web_service/ichiba/item'
+require 'rakuten_web_service/ichiba/genre'
+require 'rakuten_web_service/ichiba/ranking'
+require 'rakuten_web_service/ichiba/product'
---
+++ vendor/bundle/gems/rakuten_web_service-1.11.0/lib/rakuten_web_service/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RakutenWebService
+  VERSION = '1.11.0'.freeze
+end
